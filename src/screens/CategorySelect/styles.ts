26589dfdded2dc3cb5851136537e0729326f91ae
Denis Toledo <denis.toledo@clark.de>
2022-04-22 21:07:37 +0200

    created category select page

---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Feather } from '@expo/vector-icons';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFValue(113)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 20px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const Category = styled.View`
+  width: 100%;
+  padding: ${RFValue(16)}px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(20)}px;
+  margin-right: 16px;
+`;
+
+export const Name = styled.Text`
+  font-size: ${RFValue(14)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const Separator = styled.View`
+  width: 100%;
+  height: 1px;
+  background-color: ${({ theme }) => theme.colors.text};
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  padding: 24px;
+`;
